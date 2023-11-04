242589b59dbb511687da4d35303631015762ffb0
Sandwich5 <horia.pantelimon98@gmail.com>
2023-11-04 14:29:07 +0200

    removed token lol

--- src/main/java/net/haxjakt/demo/pmatcher/Main.java
+++ src/main/java/net/haxjakt/demo/pmatcher/Main.java
@@ -14,26 +14,16 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private static final String BOT_TOKEN = "MTE2Nzc5OTk4NjYwMDI5NjUzOA.GsDNA9.4SYQ5mK-kllYA2NTiBUb1IJnxtTUGhWCOeP40M";
     private static final Logger sLogger = LoggerFactory.getLogger(Main.class);
-    private static void initTest() {
-        BattleReportMatcher brm = new BattleReportMatcher();
-        var result = brm.toMap("Batalie pentru W-P-1 (26.10.2023 15:50:00) Unknown[V] din Vin 1 vs haxjakt[SPA] din W-P-1  --------------------------------------------------------------------------------------------- Phalanx...............................304(-5) - Phalanx................................0(-30) ............................................. - Zid.....................................1(-4) Spadasin...............................90(-0) - ............................................. Berbec.................................12(-0) - ............................................. Catapulta..............................12(-0) - ............................................. --------------------------------------------------------------------------------------------- Generali...................................-7 - Generali..................................-42 Puncte de atac............................105 - Puncte de aparare........................17.5 Daune Primite.............................350 - Daune Primite............................2100 Procent Daune.............................14% - Procent Daune.............................86% ---------------------------------------------------------------------------------------------  Castigatori: Unknown[V]  Pierzatori: haxjakt[SPA]  Atacatorul a castigat pentru ca a reusit sa sparga zidul iar aparatorul nu a avut unitati pentru a-l apara.");
-        result.entrySet().forEach(System.out::println);
-    }
     public static void main(String[] args) {
-        initTest();
-//        JDA jda = JDABuilder.createDefault(BOT_TOKEN,
-//                GatewayIntent.GUILD_MESSAGES,
-//                GatewayIntent.MESSAGE_CONTENT,
-//                GatewayIntent.GUILD_MEMBERS).build();
-//
-//        setUpSlashCommand(jda);
-//        jda.addEventListener(new CombatFormat());
-    }
+        if (args.length < 1) return;
+        JDA jda = JDABuilder.createDefault(args[0],
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MEMBERS).build();
 
-    private static void somethign() {
-        Button.success("s", "s");
+        setUpSlashCommand(jda);
+        jda.addEventListener(new CombatFormat());
     }
     private static void setUpSlashCommand(final JDA jda) {
         sLogger.info("Adding slash-commands for guild");
